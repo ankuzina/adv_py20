ffd6f1162f03463d17be9edd616e32f2e4c04008
Анна <i9777886180@gmail.com>
2020-11-23 18:15:04 +0300

    Семинар 4, упражнение 3. Напишите декоратор swap, который делает так, что задекорированная функция принимает все свои неименованные аргументы в порядке, обратном тому, в котором их передали (для аргументов с именем не вполне правильно учитывать порядок, в котором они были переданы).

---
+++ les4upr3.py
@@ -0,0 +1,15 @@
+def swap(func):
+    def wrapper(*args, **kwargs):
+        args = args[::-1]
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@swap
+def div(x, y, show=False):
+    res = x / y
+    if show:
+        print(res)
+    return res
+
+div(2, 4, show=True)
