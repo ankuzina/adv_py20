b3764dbbae34abe81653241ec37aef19b08f433c
Анна <i9777886180@gmail.com>
2020-12-31 02:12:50 +0300

    Семинар 8, упражнение 2. (Фибоначчи)

---
+++ les81z2.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    fib1, fib2 = 0, 1
+    for i in range(n):
+        fib1, fib2 = fib2, fib1 + fib2
+        yield fib1
+
+n = int(input())
+for fib in fibonacci(n):
+    print(fib, end=' ')
+print()
+
+for fib in fibonacci(10):
+    print(fib, end=' ')
+print()
+
+for fib in fibonacci(20):
+    print(fib, end=' ')
+print()
